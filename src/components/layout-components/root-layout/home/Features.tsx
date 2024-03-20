4c4581e83e5c50a1502ed8066c925554e29cbd91
habibur18 <amarhabibur@gmail.com>
2024-03-20 07:41:04 +0600

    update feature

--- src/components/layout-components/root-layout/home/Features.tsx
+++ src/components/layout-components/root-layout/home/Features.tsx
@@ -1,55 +1,62 @@
 import Image from "next/image";
-import ImageCompo from "./featuresSection/ImageCompo";
-import TextCompo from "./featuresSection/TextCompo";
 
 export default function Features() {
   return (
     <section className="container mx-auto my-10">
-      <main className="flex justify-between border">
-        <div className=" my-10 w-full space-y-[170px] border">
-          <div>
-            <div className="w-full ">
-              <div className="max-w-[53ch] mx-auto">
-                <ImageCompo />
+      <main>
+        <div className="container  mx-auto w-full h-full">
+          <div className="relative  p-10 h-full">
+            <div className="w-[5px] bg-[#2A9DF4] absolute  border-[#2A9DF4] h-full  left-[50%]">
+              <div className="w-full h-full border relative">
+                <span className="absolute w-4 h-4 top-0 left-1/2 transform -translate-x-2 bg-[#2A9DF4] rounded-full"></span>
+                <span className="absolute w-4 h-4 bottom-0 left-1/2 transform -translate-x-2 bg-[#2A9DF4] rounded-full"></span>
               </div>
             </div>
-          </div>
-          <div className="w-full flex relative justify-center items-center ">
-            <div className=" absolute top-2/5 -right-7 z-[9]  w-[48px] flex justify-center items-center  p-3  bg-[#2A9DF4] rounded-full ">
-              <figure className="">
-                <Image width={28} height={28} src={"/assets/Home/searchicon.png"} alt="criket" />
-              </figure>
-            </div>
-            <div className="w-full ">
-              <div className="max-w-[53ch] mx-auto">
-                <TextCompo />
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="w-[4px] relative bg-[#2A9DF4]">
-          <span className="absolute w-4 h-4 top-0 left-1/2 transform -translate-x-2 bg-[#2A9DF4] rounded-full"></span>
-          <span className="absolute w-4 h-4 bottom-0 left-1/2 transform -translate-x-2 bg-[#2A9DF4] rounded-full"></span>
-        </div>
 
-        <div className="my-10 w-full space-y-[170px] border border-sky-600">
-          <div className=" w-full flex relative justify-center items-center ">
-            <div className=" absolute top-2/5 -left-7 z-[8]  w-[48px] flex justify-center items-center  p-3  bg-[#2A9DF4] rounded-full ">
-              <figure className="">
-                <Image width={28} height={28} src={"/assets/Home/searchicon.png"} alt="criket" />
-              </figure>
-            </div>
-            <div className="w-full ">
-              <div className="max-w-[53ch] mx-auto">
-                <TextCompo />
+            <div className="mb-8 flex justify-between items-center w-full right-timeline">
+              <div className="order-1 w-5/12 border">
+                <div className="">
+                  <figure className="relative">
+                    <div className="">
+                      <Image className="w-full h-full lg:max-w-[444px]" width={444} height={226} src={"/assets/Home/2player.png"} alt="cricket" />
+                    </div>
+                  </figure>
+                </div>
+              </div>
+              <div className="z-20 flex items-center order-1 bg-[#2A9DF4] shadow-xl w-8 h-8 rounded-full">
+                <h1 className="mx-auto font-semibold text-lg text-white">1</h1>
+              </div>
+              <div className="order-1  rounded-lg w-5/12  py-4 border">
+                <div>
+                  <div className="text-left">
+                    <h3 className="text-[2rem] text-[#2A9DF4]">FIND LOCAL PLAYERS</h3>
+                    <p className="tracking-[0.1rem] font-[300]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div>
-            <div className="w-full ">
-              <div className="max-w-[53ch] mx-auto">
-                <ImageCompo />
+            <div className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
+              <div className="order-1 w-5/12">
+                {" "}
+                <div className="">
+                  <figure className="relative">
+                    <div className="">
+                      <Image width={444} height={226} className="w-full h-full lg:max-w-[444px]" src={"/assets/Home/2player.png"} alt="cricket" />
+                    </div>
+                  </figure>
+                </div>
+              </div>
+              <div className="z-20 flex items-center order-1 bg-[#2A9DF4] shadow-xl w-8 h-8 rounded-full">
+                <h1 className="mx-auto text-white font-semibold text-lg">2</h1>
+              </div>
+              <div className="order-1  rounded-lg  w-5/12 py-4">
+                <div>
+                  <div className="text-left">
+                    <h3 className="text-[2rem] text-[#2A9DF4]">FIND LOCAL PLAYERS</h3>
+                    <p className="tracking-[0.1rem] font-[300]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
